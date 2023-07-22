6bd857faba4993d8844ee1a1471e946ab4f0bfc8
modasser-nayem <mdalimodassernayem@gmail.com>
2023-07-22 12:19:10 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+   return (
+      <div className="App text-center">
+         <h1
+            className="text-5xl font-bold pt-14
+         "
+         >
+            Welcome to the React App
+         </h1>
+         <button className="cs-btn mt-7 text-white">Hello Programmer</button>
+      </div>
+   );
+}
+
+export default App;
